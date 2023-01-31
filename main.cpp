68869c009edabee20c6b1c2cc1c2a7438eb1074c
Farhad Asadi <34188200+frhd143@users.noreply.github.com>
2023-01-31 14:26:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
+/*
+    Written by Farhad Asadi
+*/
+
 #include <iostream>
-// Edit this class as you choose. The main function below is just there to make
-// sure the project compiles!
 
 #include "include/MediaManager.hpp"
 
